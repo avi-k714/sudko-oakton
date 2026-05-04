64f750e84a898d4c44a1313f49b179f2fdd3bcef
avi-k714 <avijit_kalra@hotmail.com>
2026-05-04 17:10:06 -0400

    Added placeholder functions in the driver_file.

---
+++ driver_file.py
@@ -0,0 +1,38 @@
+# Let's put all our code here
+
+'''
+CSC 255 OC1 - Group Assignment
+Sudoku Solver
+'''
+
+'''
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+def checkValidMove()            #Avi
+
+
+def printPuzzle()               #Thuy
+
+def checkifPuzzleComplete()     #Thuy
+
+def checkEmptyCells()           #Sebastian
+
+def solveBacktracking()         #Sebastian
+
+def candidateNumbers()          #Sebastian
+
+def generateStartingBoard()      #Avi
+
+
+
+'''
